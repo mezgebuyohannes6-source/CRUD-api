ad0a121946c804e3cfda06ef6d9615437d35e98a
Yohannes Mezgebu <mezgebuyohannes6@gmail.com>
2026-07-22 14:33:09 +0300

    add dotenv and graceful shutdown

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,32 @@
+import 'dotenv/config';
 import express from 'express';
 import taskRoutes from './routes/taskRoutes';
+import prisma from './db';
 
 
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({ message: "API is alive" });
-});
-
 app.use('/tasks', taskRoutes);
 
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+// graceful shutdown 
+const  shutdown = async (signal:string) => {
+  console.log(`\n${signal} received.shutting down gracefully...`);
+ server.close(async () => {
+  console.log('HTTP server closed.');
+  await prisma.$disconnect();
+  console.log('Prisma disconnected.');
+  process.exit(0);
+ });
+
+ setTimeout(() => {
+  console.error('Forcing shutdown after timeout.');
+  process.exit(1);
+ }, 5000);
+};
+
+process.on('SIGINT', () => shutdown('SIGINT'));
+process.on ('SIGTERM', () => shutdown( 'SIGTERM'));
